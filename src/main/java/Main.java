73d4aad33af4637ba92ad3b6de7b8b7917e3f910
Eugene <evgenijlukancikov62@gmail.com>
2025-03-26 21:16:25 +0700

    Project "Calculator"

---
+++ src/main/java/Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Пожалуйста, представьтесь: ");
+        String username = scan.nextLine();
+        System.out.println("Здравствуйте, " + username);
+
+        System.out.println("Выберите операцию: +, -, *, /");
+        String operation = scan.nextLine();
+
+        {
+            if (operation.equals("+")) {
+                System.out.println("Введите первое слагаемое");
+            }
+            String slagaemoe1 = scan.nextLine();
+            System.out.println("Введите второе слагаемое");
+            String slagaemoe2 = scan.nextLine();
+            Integer sum = Integer.parseInt(slagaemoe1) + Integer.parseInt(slagaemoe2);
+            System.out.println(sum);
+        }
+
+        {
+            if (operation.equals("-")) {
+                System.out.println("Введите уменьшаемое");
+            }
+            String num1 = scan.nextLine();
+            System.out.println("Введите вычитаемое");
+            String num2 = scan.nextLine();
+            Integer minus = Integer.parseInt(num1) - Integer.parseInt(num2);
+            System.out.println(minus);
+        }
+        {
+            if (operation.equals("*")) {
+                System.out.println("Введите первый множитель");
+            }
+            String num1 = scan.nextLine();
+            System.out.println("Введите второй множитель");
+            String num2 = scan.nextLine();
+            Integer multiplication = Integer.parseInt(num1) * Integer.parseInt(num2);
+            System.out.println(multiplication);
+        }
+        {
+            if (operation.equals("/")) {
+                System.out.println("Введите делимое");
+            }
+            String num1 = scan.nextLine();
+            System.out.println("Введите делитель");
+            String num2 = scan.nextLine();
+            if (num2 == 0) {
+                System.out.println("Error");
+            }
+        }else{
+            int division = Integer.parseInt(num1) / Integer.parseInt(num2);
+            System.out.println(division);
+        }
+    }
+}
---
+++ src/main/java/Main2.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Пожалуйста, представьтесь: ");
+        String username = scan.nextLine();
+        System.out.println("Здравствуйте, " + username);
+
+        System.out.print("Введите первое число: ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Выберите алгебраическое действие: +, -, *, /;");
+        String action = scan.nextLine();
+        action = scan.nextLine();
+        int res;
+
+        switch (action){
+
+            case "+":
+                res = num1 + num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "-":
+                res = num1 - num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "*":
+                res = num1 * num2;
+                System.out.println("Результат: " + res);
+                break;
+            case "/":
+                if (num2 == 0)
+                    System.out.println("Недопустимый делитель!");
+                else {
+                    res = num1 / num2;
+                    System.out.println("Результат: " + res);
+                    break;
+                }
+
+            {
+                System.out.println("Желаете продолжить? Введите + или - :");
+                switch()
+                {
+
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
